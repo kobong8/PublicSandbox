03d5c45b1003e6ed5d8d20b524174581a1c26550
meticulousdev <meticulousdev@gmail.com>
2024-11-13 11:33:47 +0900

    future annotations

--- pythonic/test_type_hints.py
+++ pythonic/test_type_hints.py
@@ -1,7 +1,23 @@
+from __future__ import annotations
 from typing import Dict
+
+# 02 from __future__ import annotations
+# https://docs.python.org/ko/3.12/library/__future__.html
+# future annotations 이 없을 경우 first_method에서 에러 발생
+class FirstClass:
+    def __init__(self):
+        pass
+
+    def first_method(self, input_class: SecondClass):
+        pass
+
+class SecondClass:
+    def __init__(self):
+        pass
 
 
 if __name__ == "__main__":
+    # 01 Type Hints Example
     dict_var01: Dict[str, str] = {'name', '12345678'}
     # error < python 3.9
     # Type Hinting Generics in Standard Collections
